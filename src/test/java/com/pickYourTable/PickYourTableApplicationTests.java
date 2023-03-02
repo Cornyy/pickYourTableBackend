827373f0761bff1ab8056524140f2e1759a5838e
Kornel Motyka <kornel.motyka1@gmail.com>
2023-03-03 00:50:56 +0100

    Initial commit

---
+++ src/test/java/com/pickYourTable/PickYourTableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pickYourTable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PickYourTableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
